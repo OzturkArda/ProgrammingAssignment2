173b3bb0ece01f05d4edd2af6a965bb4176658f5
OzturkArda <ardaozturrk@gmail.com>
2015-12-27 15:42:26 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This package is created for calcutalign and caching 
+## the inverse of a given (inversable) square matrix
 
-## Write a short comment describing this function
+## This function transforms a regular matrix into an enhanced
+## matrix "object" to support caching functionalities
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverseM) inv <<- inverseM
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
+## This function calculates inverse of  a "makeCacheMatrix" type matrix 
+## If "makeCacheMatrix"s inverse have already calculated it will directly 
+## return it without any calculation
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv
 }
